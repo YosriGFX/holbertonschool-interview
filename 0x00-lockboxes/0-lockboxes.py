f0a584ca29a446dcf8c90d637aa31d192947b96c
YosriGFX <YosriG1997@gmail.com>
2020-07-20 17:01:39 +0100

    0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -14,7 +14,7 @@ def canUnlockAll(boxes):
     for key in keys:
         for value in boxes[key]:
             if value not in keys:
-                if value != key:
+                if value != key and value < length and value != 0:
                     keys.append(value)
     if len(keys) == length:
         return True
