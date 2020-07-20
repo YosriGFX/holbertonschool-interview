91ec694565910db7942fc010f2f6e5845b717cad
YosriGFX <YosriG1997@gmail.com>
2020-07-20 16:59:31 +0100

    Lock Boxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''0-lockboxes'''
+
+
+def canUnlockAll(boxes):
+    ''' Can unlock ? '''
+    if type(boxes) != list:
+        return False
+    if len(boxes) == 0:
+        return False
+    else:
+        length = len(boxes)
+    keys = [0]
+    for key in keys:
+        for value in boxes[key]:
+            if value not in keys:
+                if value != key:
+                    keys.append(value)
+    if len(keys) == length:
+        return True
+    else:
+        return False
