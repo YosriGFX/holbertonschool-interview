0da3f7d63c6f29d83f2597c5ffe7b4266439abf5
YosriGFX <YosriG1997@gmail.com>
2021-03-07 00:19:08 +0100

    fixer

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "sandpiles.h"
 
 /**
  * print_grid - print_grid
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,6 +1,11 @@
 #ifndef __SANDPILES_H__
 #define __SANDPILES_H__
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
 void topple(int grid1[3][3]);
