0ed2c61e2547b006c4f31e25d88904acad05808d
YosriGFX <YosriG1997@gmail.com>
2021-03-07 00:18:03 +0100

    sandpiles.h

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef __SANDPILES_H__
+#define __SANDPILES_H__
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid(int grid[3][3]);
+void topple(int grid1[3][3]);
+void test4(int test[3][3], int grid1[3][3]);
+int stabilityTest(int grid1[3][3]);
+
+#endif __SANDPILES_H__
