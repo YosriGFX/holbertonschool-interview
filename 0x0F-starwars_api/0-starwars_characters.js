5d6040efa51b301f2cb874696d9b38665a26e5cc
YosriGFX <YosriG1997@gmail.com>
2021-06-08 19:03:49 +0100

    SemiStandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,29 +3,28 @@ const request = require('request');
 
 // Prints all characters of specified Star Wars movie
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`,
-    async (
-        error,
-        result,
-        body
-    ) => {
-        if (error) {
-            return console.log(error);
-        }
-        const chars = JSON.parse(body).characters;
-        for (const char in chars) {
-            const result = await new Promise(
-                (resolve, reject) => {
-                    request(chars[char], (error, result, html) => {
-                        if (error) {
-                            reject(error);
-                        }
-                        else {
-                            resolve(JSON.parse(html).name);
-                        }
-                    });
-                }
-            );
-            console.log(result);
-        }
+  async (
+    error,
+    result,
+    body
+  ) => {
+    if (error) {
+      return console.log(error);
     }
+    const chars = JSON.parse(body).characters;
+    for (const char in chars) {
+      const result = await new Promise(
+        (resolve, reject) => {
+          request(chars[char], (error, result, html) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(JSON.parse(html).name);
+            }
+          });
+        }
+      );
+      console.log(result);
+    }
+  }
 );
