2f2cf3e4ce235a4ad7de9dac5667ee670667d327
YosriGFX <YosriG1997@gmail.com>
2021-06-08 18:38:11 +0100

    C

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -19,44 +19,29 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		{
 			node = current->express;
 			for (temp = list; temp; temp = temp->next)
-			{
 				if (temp->next == NULL)
 				{
 					printf("Value found between indexes [%lu] and [%lu]\n",
-						current->index,
-						temp->index
-					);
+						current->index, temp->index);
 					break;
 				}
-			}
 			break;
 		}
 		if (current->express->n >= value)
 		{
 			node = current->express;
-			printf("Value checked at index [%lu] = [%d]\n",
-				node->index,
-				node->n
-			);
+			printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
 			printf("Value found between indexes [%lu] and [%lu]\n",
-				current->index,
-				node->index
-			);
+				current->index, node->index);
 			break;
 		}
 		current = current->express;
-		printf("Value checked at index [%lu] = [%d]\n",
-			current->index,
-			current->n
-		);
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
 	}
 
 	while (current)
 	{
-		printf("Value checked at index [%lu] = [%d]\n",
-			current->index,
-			current->n
-		);
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
 		if (current->n == value)
 			return (current);
 		current = current->next;
