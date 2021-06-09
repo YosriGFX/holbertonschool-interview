e4a3dcd317de99803ecfa72d1498984bbec2b91e
YosriGFX <YosriG1997@gmail.com>
2021-06-09 10:58:26 +0100

    🚨

---
+++ 0x16-rotate_2d_matrix/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Test 0x16 - Rotate 2D Matrix
+"""
+rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+    for ma in matrix:
+      print(ma)
+    print('---')
+    rotate_2d_matrix(matrix)
+    for ma in matrix:
+      print(ma)
