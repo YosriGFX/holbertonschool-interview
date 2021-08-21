86f2d8a81f4a3250363feaa3f60cbfb9028847d6
YosriGFX <YosriG1997@gmail.com>
2021-08-21 16:31:20 +0100

    0-radix_sort.c - YosriGFX

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "sort.h"
+
+
+/**
+ * radix_sort - the Radix sort algorithm
+ * @array: array
+ * @size: array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int i, j, temp, min, holder;
+	int div = 10;
+	size_t a, count;
+
+	count = size;
+	if (!array || size == 0)
+		return;
+	for (a = 0; a < size - 1; a++)
+	{
+		if (array[a] > array[a + 1])
+		{
+			for (i = 0; i < count; i++)
+			{
+				holder = i;
+				min = array[i] % div;
+				for (j = i + 1; j < count; j++)
+				{
+					if (min > (array[j] % div))
+					{
+						min = array[j] % div;
+						holder = j;
+					}
+				}
+				temp = array[holder];
+				array[holder] = array[i];
+				array[i] = temp;
+			}
+			print_array(array, size);
+			div *= 10;
+		}
+	}
+}
