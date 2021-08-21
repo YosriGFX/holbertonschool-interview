9ce4c952afe3a1c953b6342c401834519cd4ff7c
YosriGFX <YosriG1997@gmail.com>
2021-08-21 23:15:21 +0100

    0-island_perimeter.py

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -5,9 +5,16 @@
 def island_perimeter(grid):
     '''returns the perimeter of
     the island described in grid'''
-    cells = 0
-    for row in grid:
-        for column in row:
-            if column == 1:
-                cells += 1
-    return ((cells * 1) * 2) + 2
+    perimeter = 0
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 1:
+                if row - 1 < 0 or grid[row - 1][column] == 0:
+                    perimeter += 1
+                if row + 1 >= len(grid) or grid[row + 1][column] == 0:
+                    perimeter += 1
+                if column - 1 < 0 or grid[row][column - 1] == 0:
+                    perimeter += 1
+                if column + 1 >= len(grid[row]) or grid[row][column + 1] == 0:
+                    perimeter += 1
+    return perimeter
